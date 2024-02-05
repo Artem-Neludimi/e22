550cde9821f1311f618c28e18a6d32a8d38dd7fd
artem <art.zavtur@gmail.com>
2024-02-05 12:22:54 +0200

    support

--- lib/presentation/sbp_settings_jus.dart
+++ lib/presentation/sbp_settings_jus.dart
@@ -3,6 +3,7 @@ import 'package:e22/core/navigation/sbp_router_jus.dart';
 import 'package:e22/presentation/sbp_widgets_jus.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:gradient_borders/gradient_borders.dart';
 import 'package:rate_my_app/rate_my_app.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -66,6 +67,28 @@ class _SbpSettingsJusState extends State<SbpSettingsJus> {
                   ),
                   const SizedBox(height: 12),
                   _item(
+                    onTap: () {
+                      showDialog(
+                        context: context,
+                        builder: (context) => const _SupportDialog(),
+                      ).then((value) {
+                        if (value == true) {
+                          showCupertinoDialog(
+                            context: context,
+                            builder: (context) => CupertinoAlertDialog(
+                              title: const Text('Message sent'),
+                              content: const Text('We will get back to you as soon as possible'),
+                              actions: [
+                                CupertinoDialogAction(
+                                  child: const Text('OK'),
+                                  onPressed: () => context.pop(),
+                                ),
+                              ],
+                            ),
+                          );
+                        }
+                      });
+                    },
                     'Support',
                     const Icon(
                       Icons.arrow_forward_ios,
@@ -198,6 +221,106 @@ class _SbpSettingsJusState extends State<SbpSettingsJus> {
         style: context.sourceCode(),
       ),
       trailing: trailing,
+    );
+  }
+}
+
+class _SupportDialog extends StatefulWidget {
+  const _SupportDialog();
+
+  @override
+  State<_SupportDialog> createState() => _SupportDialogState();
+}
+
+class _SupportDialogState extends State<_SupportDialog> {
+  final _controller = TextEditingController();
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        const SizedBox(height: 33),
+        Material(
+          child: Container(
+            width: 300,
+            height: 275,
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  Color.fromRGBO(152, 119, 70, 1),
+                  Color.fromRGBO(53, 44, 31, 1),
+                ],
+              ),
+              border: GradientBoxBorder(
+                width: 4,
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    Color.fromRGBO(224, 176, 104, 1),
+                    Color.fromRGBO(78, 56, 38, 1),
+                  ],
+                ),
+              ),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(8),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const Text(
+                    'Support',
+                    style: TextStyle(
+                      color: Color.fromRGBO(221, 168, 95, 1),
+                      fontSize: 33,
+                    ),
+                  ),
+                  const SizedBox(height: 24),
+                  Expanded(
+                    child: TextField(
+                      controller: _controller,
+                      onTapOutside: (_) => FocusManager.instance.primaryFocus?.unfocus(),
+                      cursorColor: Colors.white,
+                      style: context.sourceSans(),
+                      maxLines: 30,
+                      decoration: InputDecoration(
+                        hintText: 'Describe your problem',
+                        hintStyle: context.sourceSans(),
+                        filled: true,
+                        fillColor: const Color.fromRGBO(30, 30, 30, 0.5),
+                        border: const OutlineInputBorder(
+                          borderSide: BorderSide.none,
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+        const SizedBox(height: 24),
+        SizedBox(
+          width: 300,
+          child: Material(
+            child: SbpButtonJus(
+              onPressed: () {
+                if (_controller.text.isEmpty) return;
+                context.pop(true);
+              },
+              text: 'Send message',
+            ),
+          ),
+        )
+      ],
     );
   }
 }
